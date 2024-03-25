3541962e4eec999df61256fb7472755090ef5a17
Salam747 <159710405+Salam747@users.noreply.github.com>
2024-03-25 15:52:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+// import inquirer from "inquirer";
+export {};
+//   const randomNumber = Math.floor(Math.random()*6+1);
+//  const answers = await inquirer.prompt([
+//    {
+//     name: "userGuessNumber",
+//     type: "number",
+//        message: "Please guess a number 1-6>",
+//     },
+// ]);
+// if (answers.userGuessNumber === randomNumber) {
+//     console.log("Gongratulations! you guessed right number");
+//   } else {
+//    console.log("you guessed wrong number");
+//   }
---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+// import inquirer from "inquirer";
+
+//   const randomNumber = Math.floor(Math.random()*6+1);
+
+//  const answers = await inquirer.prompt([
+//    {
+//     name: "userGuessNumber",
+//     type: "number",
+//        message: "Please guess a number 1-6>",
+//     },
+// ]);
+
+// if (answers.userGuessNumber === randomNumber) {
+//     console.log("Gongratulations! you guessed right number");
+//   } else {
+//    console.log("you guessed wrong number");
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
